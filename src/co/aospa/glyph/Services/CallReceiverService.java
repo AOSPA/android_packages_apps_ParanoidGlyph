a9eba907ece2a1c905cf2e2b1c2569dadd85ca49
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-01-18 18:40:09 +0100

    ParanoidGlyph: Revert back to old CallReceiverService method for calls
    This reverts commit 1e8905cc87650577ff240b297d238ae68cc49d19.
    
    Change-Id: Ia839829cb51863d4d2b3076c88098f6544fd4857

--- src/co/aospa/glyph/Services/CallReceiverService.java
+++ src/co/aospa/glyph/Services/CallReceiverService.java
@@ -17,15 +17,13 @@
 package co.aospa.glyph.Services;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
+import android.content.IntentFilter;
 import android.os.IBinder;
+import android.telephony.TelephonyManager;
 import android.util.Log;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import co.aospa.glyph.Manager.AnimationManager;
 
@@ -33,18 +31,14 @@ public class CallReceiverService extends Service {
 
     private static final String TAG = "GlyphCallReceiverService";
     private static final boolean DEBUG = true;
-    private AudioManager mAudioManager;
-    private ExecutorService mExecutorService;
 
     @Override
     public void onCreate() {
         if (DEBUG) Log.d(TAG, "Creating service");
 
-        mExecutorService = Executors.newSingleThreadExecutor();
-
-        mAudioManager = getSystemService(AudioManager.class);
-        mAudioManager.addOnModeChangedListener(mExecutorService, mAudioManagerOnModeChangedListener);
-        mAudioManagerOnModeChangedListener.onModeChanged(mAudioManager.getMode());
+        IntentFilter callReceiver = new IntentFilter();
+        callReceiver.addAction(TelephonyManager.ACTION_PHONE_STATE_CHANGED);
+        registerReceiver(mCallReceiver, callReceiver);
     }
 
     @Override
@@ -56,8 +50,8 @@ public class CallReceiverService extends Service {
     @Override
     public void onDestroy() {
         if (DEBUG) Log.d(TAG, "Destroying service");
+        this.unregisterReceiver(mCallReceiver);
         disableCallAnimation();
-        mAudioManager.removeOnModeChangedListener(mAudioManagerOnModeChangedListener);
         super.onDestroy();
     }
 
@@ -76,14 +70,23 @@ public class CallReceiverService extends Service {
         AnimationManager.stopCall();
     }
 
-    private AudioManager.OnModeChangedListener mAudioManagerOnModeChangedListener = new AudioManager.OnModeChangedListener() {
+    private BroadcastReceiver mCallReceiver = new BroadcastReceiver() {
         @Override
-        public void onModeChanged(int mode) {
-            if (DEBUG) Log.d(TAG, "mAudioManagerOnModeChangedListener: " + mode);
-            if (mode == AudioManager.MODE_RINGTONE) {
-                enableCallAnimation();
-            } else {
-                disableCallAnimation();
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
+                String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+                if(state.equals(TelephonyManager.EXTRA_STATE_RINGING)){
+                    if (DEBUG) Log.d(TAG, "EXTRA_STATE_RINGING");
+                    enableCallAnimation();
+                }
+                if ((state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK))){
+                    if (DEBUG) Log.d(TAG, "EXTRA_STATE_OFFHOOK");
+                    disableCallAnimation();
+                }
+                if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)){
+                    if (DEBUG) Log.d(TAG, "EXTRA_STATE_IDLE");
+                    disableCallAnimation();
+                }
             }
         }
     };

b9cf7125d256bfb34e0bc8e6d8e59b3e35d4d8bb
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-01-19 08:43:03 +0100

    ParanoidGlyph: Add back AudioManager for better incall detection
     *  Using AudioManager in combination with CallReceiverService allows us to cover VoIP and normal calls in most cases.
     * In some cases the animation gets started or stopped twice, but AnimationManager takes care of that.
    
    Change-Id: If9573bc9bbffbff64149beb8906e35a0cf584b66

--- src/co/aospa/glyph/Services/CallReceiverService.java
+++ src/co/aospa/glyph/Services/CallReceiverService.java
@@ -21,9 +21,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.AudioManager;
 import android.os.IBinder;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+
+import java.util.concurrent.Executors;
 
 import co.aospa.glyph.Manager.AnimationManager;
 
@@ -32,9 +35,15 @@ public class CallReceiverService extends Service {
     private static final String TAG = "GlyphCallReceiverService";
     private static final boolean DEBUG = true;
 
+    private AudioManager mAudioManager;
+
     @Override
     public void onCreate() {
         if (DEBUG) Log.d(TAG, "Creating service");
+
+        mAudioManager = getSystemService(AudioManager.class);
+        mAudioManager.addOnModeChangedListener(Executors.newSingleThreadExecutor(), mAudioManagerOnModeChangedListener);
+        mAudioManagerOnModeChangedListener.onModeChanged(mAudioManager.getMode());
 
         IntentFilter callReceiver = new IntentFilter();
         callReceiver.addAction(TelephonyManager.ACTION_PHONE_STATE_CHANGED);
@@ -51,6 +60,7 @@ public class CallReceiverService extends Service {
     public void onDestroy() {
         if (DEBUG) Log.d(TAG, "Destroying service");
         this.unregisterReceiver(mCallReceiver);
+        mAudioManager.removeOnModeChangedListener(mAudioManagerOnModeChangedListener);
         disableCallAnimation();
         super.onDestroy();
     }
@@ -87,6 +97,18 @@ public class CallReceiverService extends Service {
                     if (DEBUG) Log.d(TAG, "EXTRA_STATE_IDLE");
                     disableCallAnimation();
                 }
+            }
+        }
+    };
+
+    private final AudioManager.OnModeChangedListener mAudioManagerOnModeChangedListener = new AudioManager.OnModeChangedListener() {
+        @Override
+        public void onModeChanged(int mode) {
+            if (DEBUG) Log.d(TAG, "mAudioManagerOnModeChangedListener: " + mode);
+            if (mode == AudioManager.MODE_RINGTONE) {
+                enableCallAnimation();
+            } else {
+                disableCallAnimation();
             }
         }
     };

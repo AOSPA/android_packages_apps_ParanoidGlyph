07a55014fec01f92dbdd046a5cbf1be044bddd54
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-01-12 15:36:30 +0100

    ParanoidGlyph: Final changes for Phone (2)
    Change-Id: I625f111b4b3c4a1c4ab35dbc7163b34831df57d0

--- src/co/aospa/glyph/Tiles/GlyphTileService.java
+++ src/co/aospa/glyph/Tiles/GlyphTileService.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
  * Copyright (C) 2017 The LineageOS Project
- * Copyright (C) 2022-2023 Paranoid Android
+ * Copyright (C) 2022-2024 Paranoid Android
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

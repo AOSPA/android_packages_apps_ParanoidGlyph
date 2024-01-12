392753c9b97beea7d4b113b040be17b04786f835
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-01-12 15:36:30 +0100

    ParanoidGlyph: Final changes for Phone (2)
    Change-Id: I625f111b4b3c4a1c4ab35dbc7163b34831df57d0

--- src/co/aospa/glyph/Services/FlipToGlyphService.java
+++ src/co/aospa/glyph/Services/FlipToGlyphService.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
  *               2017-2018 The LineageOS Project
- *               2020-2023 Paranoid Android
+ *               2020-2024 Paranoid Android
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

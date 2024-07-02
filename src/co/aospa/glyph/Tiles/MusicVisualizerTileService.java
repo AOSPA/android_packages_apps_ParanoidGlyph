12317e8fbc011ca4c30eb76e2d7acf77b65eb376
Abhay Singh Gill <abhaygill017@gmail.com>
2024-07-02 21:08:36 +0530

    ParanoidGlyph: Add music visualizer tile
    Change-Id: I76c52e841bf7b42eb8822ffd901b91cd19fe80cd
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

---
+++ src/co/aospa/glyph/Tiles/MusicVisualizerTileService.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2022-2024 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.aospa.glyph.Tiles;
+
+import android.content.SharedPreferences; 
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import androidx.preference.PreferenceManager;
+
+import co.aospa.glyph.Constants.Constants;
+import co.aospa.glyph.R;
+import co.aospa.glyph.Manager.SettingsManager;
+import co.aospa.glyph.Utils.ServiceUtils;
+
+/** Quick settings tile: Glyph **/
+public class MusicVisualizerTileService extends TileService {
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        updateState();
+    }
+
+    private void updateState() {
+        boolean enabled = getEnabled();
+        getQsTile().setContentDescription(enabled ?
+                getString(R.string.glyph_accessibility_quick_settings_on) :
+                getString(R.string.glyph_accessibility_quick_settings_off));
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        setEnabled(!getEnabled());
+        updateState();
+    }
+
+    private boolean getEnabled() {
+        return SettingsManager.isGlyphMusicVisualizerEnabled();
+    }
+
+    private void setEnabled(boolean enabled) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(Constants.CONTEXT);
+        sharedPrefs.edit().putBoolean(Constants.GLYPH_MUSIC_VISUALIZER_ENABLE, enabled).apply();
+        ServiceUtils.checkGlyphService();
+    }
+}
